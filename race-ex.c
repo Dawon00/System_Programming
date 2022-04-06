61ad91ff65d81fbf19ce350f0100346dd975c2d6
Dawon00 <71630722+Dawon00@users.noreply.github.com>
2022-04-07 00:51:04 +0900

    Create race-ex.c

---
+++ race-ex.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, n;
+	char buf;
+	fd1 = open("myfile", O_RDONLY);
+	fd2 = creat("copyfile",0666);
+	
+	fork();
+	
+	while((n=read(fd1, &buf,1)) != 0)
+		write(fd2, &buf, 1);
+	close(fd1);
+	clsoe(fd2);
+}
